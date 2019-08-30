9ec8cc212561ae5e2d1c67a59c56ea078bbbc1b0
Isaac Lewis <isaac.b.lewis@gmail.com>
2019-08-30 15:19:25 -0400

    Move code that really should belong in serio-driver.scm to that file; remove unneeded code; simplify the start procedure.

--- scheme/izzy/driver.scm
+++ scheme/izzy/driver.scm
@@ -14,12 +14,9 @@
 (define-once izzy-debug #f)
 
 (define-class <driver> ()
-  (main-thread #:init-value #f #:accessor main-thread)
+  (driver-thread #:init-value #f #:accessor driver-thread)
   (input-port #:init-value #f #:accessor input-port)
-  (prev-code #:init-value 0 #:accessor prev-code)
-  (ignored-input-codes #:init-value '() #:accessor ignored-input-codes)
-  (input-codes #:init-value '() #:accessor input-codes)
-  (conversion-fn #:init-value #f #:accessor conversion-fn)
+  (read-kie-func #:init-value (lambda (x) (error "this must be set")) #:accessor read-kie-func)
   (notify-pipe #:init-value #f #:accessor notify-pipe)
   (event-queue #:init-value #f #:accessor event-queue))
 
@@ -34,60 +31,31 @@
 
 (define-method (initialize (obj <driver>) initargs)
   ;; TODO: add type checking
-  (slot-set! obj 'main-thread #f)
+  (slot-set! obj 'driver-thread #f)
   (set-input-port! obj (first initargs))
-  (slot-set! obj 'ignored-input-codes (second initargs))
-  (slot-set! obj 'input-codes (third initargs))
-  (slot-set! obj 'conversion-fn (fourth initargs))
-  (if (> (length initargs) 4)
-      (slot-set! obj 'notify-pipe (list-ref initargs 4))
+  (slot-set! obj 'read-kie-func (second initargs))
+  ;; list-ref regards the first element as being at position 0
+  (if (> (length initargs) 2)
+      (slot-set! obj 'notify-pipe (list-ref initargs 2))
       (slot-set! obj 'notify-pipe #f))
-  (if (> (length initargs) 5)
-      (slot-set! obj 'event-queue (list-ref initargs 5))
+  (if (> (length initargs) 3)
+      (slot-set! obj 'event-queue (list-ref initargs 3))
       (slot-set! obj 'event-queue #f)))
-
-(define-method (read-kernel-input-event (dr <driver>))
-  "Attempts to read a complete input event from the given driver."
-  (letrec ((port2 (input-port dr))
-	   (helper
-	    (lambda (cur-code depth)
-	      (cond ((>= depth 10)
-		     (logln #t "Warning: unknown code ~a\n" cur-code)
-		     (logln #t "Discarding input: ~a\n" (drain-input port2))
-		     (helper (get-u8 port2) 0))
-		    ((member cur-code (ignored-input-codes dr))
-		     (logln #t "Ignoring: code ~a\n" cur-code)
-		     (helper (get-u8 port2) 0))
-		    ((member cur-code (input-codes dr))
-		     ;;(logln #t "raw code:~a\n" cur-code)
-		     cur-code)
-		    (else
-		     (helper (+ (* 1000 cur-code) (get-u8 port2)) (+ 1 depth)))))))
-    (helper (get-u8 port2) 0)))
-
-
-
 
 ;;; Public methods
 
 (define-method (start (dr <driver>))
-  (slot-set! dr 'main-thread
+  (slot-set! dr 'driver-thread
 	     (call-with-new-thread
 	      (lambda ()
-		(let ((prev-code 0))
 		  (while #t
-			 (let* ((cur-code (read-kernel-input-event dr))
-				(kie ((conversion-fn dr) prev-code cur-code)))
+			 (let* ((kie ((read-kie-func dr) dr)))
 			   (when (and (notify-pipe dr)
 				      (event-queue dr))
 			     (enq! (event-queue dr) kie)
 			     (write-char #\r (cdr (notify-pipe dr)))
-			     (force-output (cdr (notify-pipe dr)))
-			     ;; (logln #t "0) prev-code: ~a kie: ~a"
-			     ;; 	       prev-code
-			     ;; 	       kie)
-			     (set! prev-code cur-code)))))))))
+			     (force-output (cdr (notify-pipe dr))))))))))
 
 (define-method (stop (dr <driver>))
-  (when (thread? (slot-ref dr 'main-thread))
-    (cancel-thread (slot-ref dr 'main-thread))))
+  (when (thread? (slot-ref dr 'driver-thread))
+    (cancel-thread (slot-ref dr 'driver-thread))))
